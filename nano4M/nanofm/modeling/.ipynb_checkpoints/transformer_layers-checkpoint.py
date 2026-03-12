a64a0759a11008ee3d9a1998b1a31f2a54bae6a3
Jules <jules.silvestre@epfl.ch>
2026-03-12 09:27:26 +0100

    NanoGPT: first implementation

---
+++ nano4M/nanofm/modeling/.ipynb_checkpoints/transformer_layers-checkpoint.py
@@ -0,0 +1,322 @@
+# Copyright 2025 EPFL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# --------------------------------------------------------
+# Some functions are based on the timm and 4M code bases
+# https://github.com/huggingface/pytorch-image-models
+# https://github.com/apple/ml-4m
+# --------------------------------------------------------
+
+from typing import Optional
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+
+class LayerNorm(nn.Module):
+    """Custom implementation of LayerNorm with the option to disable the bias term."""
+    def __init__(self, normalized_shape: int, eps: float = 1e-6, bias: bool = False):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Parameter(torch.ones(normalized_shape))
+        if bias:
+            self.bias = nn.Parameter(torch.zeros(normalized_shape))
+        else:
+            self.register_buffer("bias", torch.zeros(normalized_shape))
+
+        # Normalized shape must be a tuple for F.layer_norm
+        self.normalized_shape = (normalized_shape,)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return nn.functional.layer_norm(x, self.normalized_shape, self.weight, self.bias, eps=self.eps)
+
+
+class Mlp(nn.Module):
+    """
+    MLP module with GELU activation.
+
+    Args:
+        in_features: Number of input features
+        hidden_features: Number of hidden features (optional)
+        out_features: Number of output features (optional)
+        bias: Whether to include bias in the linear layers
+    """
+    def __init__(self, 
+            in_features: int, 
+            hidden_features: Optional[int] = None, 
+            out_features: Optional[int] = None, 
+            bias: bool = False,
+        ):
+        super().__init__()
+        out_features = out_features or in_features
+        hidden_features = hidden_features or in_features
+        
+        self.fc1 = nn.Linear(in_features, hidden_features, bias=bias)
+        self.act = nn.GELU()
+        self.fc2 = nn.Linear(hidden_features, out_features, bias=bias)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.fc1(x)
+        x = self.act(x)
+        x = self.fc2(x)
+        return x
+
+
+class Attention(nn.Module):
+    """
+    Multi-head self-attention module.
+
+    Args:
+        dim: Transformer dimension
+        head_dim: Dimension of each attention head
+        qkv_bias: Whether to include bias in the QKV linear layers
+        proj_bias: Whether to include bias in the attention output projection
+    """
+    def __init__(self, dim: int, head_dim: int = 64, qkv_bias: bool = False, proj_bias: bool = False):
+        super().__init__()
+        self.num_heads = dim // head_dim
+        self.scale = head_dim ** -0.5
+
+        self.qkv = nn.Linear(in_features, 3 * in_features, bias=bias)
+        self.attn_out_proj = nn.Linear(dim, dim, bias=proj_bias)
+
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        B, L, D = x.shape # Batch size, sequence length, and dimension
+
+        # Shape [B, L, D]
+        qkv = self.qkv(x)
+        q, k, v = qkv.chunk(3, dim = -1)
+        
+        head_dim = D // self.num_heads
+
+        # Reshaping to: [B, num_heads, L, head_dim]
+        q = q.reshape(B, L, self.num_heads, head_dim).transpose(1, 2)
+        k = k.reshape(B, L, self.num_heads, head_dim).transpose(1, 2)
+        v = v.reshape(B, L, self.num_heads, head_dim).transpose(1, 2)
+
+        attn = (q @ k.transpose(-2, -1)) * self.scale
+
+        if mask is not None:
+            mask = rearrange(mask, "b n m -> b 1 n m") # Unsqueeze for multi-head attention
+            # Apply the optional attention mask. Wherever the mask is False, replace the attention 
+            # matrix value by negative infinity → zero attention weight after softmax.
+            attn = attn.masked_fill(~mask, float("-inf")) # ~mask invert attn so we only put -inf where it's False
+
+        # Compute the softmax over the last dimension
+        attn = torch.softmax(attn, dim=-1)
+
+        # Weight the values V by the attention matrix and concatenate the different attention heads
+        # Make sure to reshape the output to the original shape of x, i.e. [B L D]
+        x = (attn @ v).transpose(1, 2).reshape(B, L, D)
+
+        # Output projection
+        x = self.attn_out_proj(x)
+        return x
+
+
+class CrossAttention(nn.Module):
+    """
+    Multi-head cross-attention module.
+
+    Args:
+        dim: Transformer dimension
+        head_dim: Dimension of each attention head
+        qkv_bias: Whether to include bias in the QKV linear layers
+        proj_bias: Whether to include bias in the attention output projection
+    """
+    def __init__(self, dim: int, head_dim: int = 64, qkv_bias: bool = False, proj_bias: bool = False):
+        super().__init__()
+        self.num_heads = dim // head_dim
+        self.scale = head_dim ** -0.5
+
+        # TODO: Define here the linear layer producing Q from the input x
+        ???
+
+        # TODO: Define here the linear layers producing K, V from the context
+        # Hint: Do you need to define two different projections, or can you use a single one for both?
+        ???
+
+        self.attn_out_proj = nn.Linear(dim, dim, bias=proj_bias)
+
+    def forward(self, x: torch.Tensor, context: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        B, N, C = x.shape # Batch size, x sequence length (N), and dimension
+        _, M, _ = context.shape # _, context sequence length (M), _
+
+        # TODO: Compute the queries Q from x. It should be of shape [B num_heads N head_dim].
+        q = ???
+
+        # TODO: Compute the keys K and values V from the context. Each should be of shape [B num_heads M head_dim].
+        q = ???
+
+        # TODO: Compute the attention matrix (pre softmax) and scale it by 1/sqrt(d_k). It should be of shape [B num_heads N M].
+        # Hint: Use the already defined self.scale
+        attn = ???
+
+        if mask is not None:
+            mask = rearrange(mask, "b n m -> b 1 n m") # Unsqueeze for multi-head attention
+            # TODO: Apply the optional attention mask. Wherever the mask is False, replace the attention 
+            # matrix value by negative infinity → zero attention weight after softmax.
+            attn = ???
+
+        # TODO: Compute the softmax over the last dimension
+        attn = ???
+
+        # TODO: Weight the values V by the attention matrix and concatenate the different attention heads
+        # Make sure to reshape the output to the original shape of x, i.e. [B N D]
+        x = ???
+        
+        # Output projection
+        x = self.attn_out_proj(x)
+
+        return x
+
+
+class Block(nn.Module):
+    """
+    Basic transformer block with a multi-head self-attention mechanism and a feed-forward MLP.
+
+    Args:
+        dim: Transformer dimension
+        head_dim: Dimension of each attention head
+        mlp_ratio: Ratio of MLP hidden dimension to transformer dimension
+        use_bias: Whether to include bias in the QKV, attention output projection and MLP layers
+    """
+    def __init__(self, dim: int, head_dim: int = 64, mlp_ratio: float = 4., use_bias: bool = False):
+        super().__init__()
+        self.norm1 = LayerNorm(dim, bias = use_bias)
+        self.attn = Attention(dim=dim, head_dim=head_dim, bias=use_bias)
+        self.norm2 = LayerNorm(dim, bias = use_bias)
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = MLP(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            out_features=dim,
+            bias=use_bias,
+        )
+
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        x = x + self.attn(self.norm1(x), mask)
+        x = x + self.mlp(self.norm2(x))
+        return x
+
+
+class DecoderBlock(nn.Module):
+    """
+    Basic transformer decoder block with a multi-head self-attention, 
+    a multi-head cross-attention, and a feed-forward MLP layer.
+
+    Args:
+        dim: Transformer dimension
+        head_dim: Dimension of each attention head
+        mlp_ratio: Ratio of MLP hidden dimension to transformer dimension
+        use_bias: Whether to include bias in the QKV, attention output projection and MLP layers
+    """
+    def __init__(self, dim: int, head_dim: int = 64, mlp_ratio: float = 4., use_bias: bool = False):
+        super().__init__()
+        self.norm1 = ??? # TODO (use the LayerNorm defined above)
+        self.query_norm = ??? # TODO (use the LayerNorm defined above)
+        self.context_norm = ??? # TODO (use the LayerNorm defined above)
+        self.norm2 = ??? # TODO (use the LayerNorm defined above)
+
+        self.self_attn = ??? # TODO Attention layer
+        self.cross_attn = ??? # TODO CrossAttention layer
+
+        mlp_hidden_dim = int(dim * mlp_ratio)
+        self.mlp = ??? # TODO MLP layer
+
+    def forward(self, 
+            x: torch.Tensor, 
+            context: torch.Tensor, 
+            sa_mask: Optional[torch.Tensor] = None, # Self-attention mask
+            xa_mask: Optional[torch.Tensor] = None, # Cross-attention mask
+        ) -> torch.Tensor:
+
+        # Self-attention, then cross-attention, then MLP
+        # Make sure to apply the self-attention mask (sa_mask) to the self-attention layer,
+        # and the cross-attention mask (xa_mask) to the cross-attention layer.
+        # Don't forget to add the residual connections after each layer, and
+        # to apply the normalizations on the inputs of each layer.
+        ??? # TODO
+
+
+class TransformerTrunk(nn.Module):
+    """Basic Transformer trunk definition that can be used for encoder-only,
+    decoder-only and prefixLM models, depending on the attention mask applied.
+
+    Args:
+        dim: Transformer dimension
+        depth: Number of transformer layers
+        head_dim: Dimension of each attention head
+        mlp_ratio: Ratio of MLP hidden dimension to transformer dimension
+        use_bias: Whether to include bias in the QKV, attention output projection and MLP layers
+    """
+    def __init__(
+        self,
+            dim: int = 512,
+            depth: int = 8,
+            head_dim: int = 64,
+            mlp_ratio: float = 4.0,
+            use_bias: bool = False,
+        ):
+        super().__init__()
+
+        self.blocks = nn.ModuleList(
+            [Block(dim=dim, head_dim=head_dim, mlp_ratio=mlp_ratio, use_bias=use_bias) 
+            for _ in range(depth)]
+        )
+    
+    def forward(self, x: torch.Tensor, mask: Optional[torch.Tensor] = None) -> torch.Tensor:
+        
+        for block in self.blocks:
+            x = block(x, mask=mask)
+        return x
+
+
+class TransformerDecoderTrunk(nn.Module):
+    """Basic Transformer decoder with interleaved self- and cross-attention, that can
+    be used as the decoder for encoder-decoder models.
+
+    Args:
+        dim: Transformer dimension
+        depth: Number of transformer layers
+        head_dim: Dimension of each attention head
+        mlp_ratio: Ratio of MLP hidden dimension to transformer dimension
+        use_bias: Whether to include bias in the QKV, attention output projection and MLP layers
+    """
+    def __init__(
+        self,
+            dim: int = 512,
+            depth: int = 8,
+            head_dim: int = 64,
+            mlp_ratio: float = 4.0,
+            use_bias: bool = False,
+        ):
+        super().__init__()
+
+        self.blocks = nn.ModuleList([
+                DecoderBlock(dim=dim, head_dim=head_dim, mlp_ratio=mlp_ratio, use_bias=use_bias)
+                for _ in range(depth)]
+        )
+    
+    def forward(
+            self, 
+            x: torch.Tensor, 
+            context: torch.Tensor, 
+            sa_mask: Optional[torch.Tensor] = None, # Self-attention mask
+            xa_mask: Optional[torch.Tensor] = None, # Cross-attention mask
+        ) -> torch.Tensor:
+        
+        for block in self.blocks:
+            x = block(x, context, sa_mask=sa_mask, xa_mask=xa_mask)
+        return x
---
+++ nano4M/nanofm/models/.ipynb_checkpoints/gpt-checkpoint.py
@@ -0,0 +1,226 @@
+# Copyright 2025 EPFL
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Dict, Any, List, Optional
+import math
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from einops import rearrange
+
+from nanofm.modeling.transformer_layers import TransformerTrunk, LayerNorm
+from nanofm.utils.sampling import sample_tokens
+
+
+class GPT(nn.Module):
+    """Basic autoregressive Transformer definition.
+
+    Args:
+        seq_read_key: Key in the data_dict corresponding to the full sequence
+        dim: Transformer dimension
+        depth: Number of transformer layers
+        head_dim: Dimension of each attention head
+        mlp_ratio: Ratio of MLP hidden dimension to transformer dimension
+        use_bias: Whether to include bias in the QKV, attention output projection and MLP layers
+        vocab_size: Size of the vocabulary
+        max_seq_len: Maximum sequence length
+        padding_idx: Index of the padding token, [PAD], in the vocabulary
+        init_std: Standard deviation for weight initialization
+    """
+    def __init__(
+            self,
+            seq_read_key: str = 'input_ids',
+            dim: int = 512,
+            depth: int = 8,
+            head_dim: int = 64,
+            mlp_ratio: float = 4.0,
+            use_bias: bool = False,
+            vocab_size: int = 10000,
+            max_seq_len: int = 256,
+            padding_idx: int = -100,
+            init_std: float = 0.02,
+        ):
+        super().__init__()
+        self.seq_read_key = seq_read_key
+        self.padding_idx = padding_idx
+        self.max_seq_len = max_seq_len
+        self.init_std = init_std
+
+        self.input_embedding = nn.Embedding(vocab_size, dim, padding_idx=padding_idx)
+        self.positional_embedding = nn.Parameter(torch.rand(max_seq_len, dim))
+        
+        self.trunk = TransformerTrunk(dim, depth, head_dim, mlp_ratio, use_bias)
+        
+        self.out_norm = LayerNorm(dim)
+        self.to_logits = nn.Linear(dim, vocab_size, bias=False)
+
+        self.initialize_weights() # Weight initialization
+
+    @property
+    def device(self):
+        return next(self.parameters()).device
+
+    def initialize_weights(self):
+        """Initialize the weights of the model.""" 
+        self.apply(self._init_weights) # Initialize nn.Linear and nn.Embedding
+        nn.init.normal_(self.positional_embedding, mean=0.0, std=self.init_std) # Initialize the positional embeddings
+        nn.init.constant_(self.to_logits.weight, 0) # Zero-init the output projection
+
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            nn.init.normal_(module.weight, mean=0.0, std=self.init_std)
+            if module.bias is not None:
+                nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            nn.init.normal_(module.weight, mean=0.0, std=self.init_std)
+
+    def get_num_params(self, non_embedding=True):
+        """
+        Return the number of parameters in the model.
+
+        Args:
+            non_embedding: For non-embedding count (default), the input and output embeddings get subtracted.
+        Returns:
+            The number of parameters in the model.
+        """
+        n_params = sum(p.numel() for p in self.parameters())
+        if non_embedding:
+            n_params -= self.input_embedding.weight.numel()
+            n_params -= self.positional_embedding.numel()
+            n_params -= self.to_logits.weight.numel()
+        return n_params
+    
+    def forward_model(self, x: torch.LongTensor) -> torch.Tensor:
+        """
+        Forward pass through the model.
+
+        Args:
+            x: LongTensor of shape (B, L) containing the input token indices.
+        Returns:
+            Tensor of shape (B, L, vocab_size) containing the logits.
+        """
+        B, L = x.size() # batch size and sequence length
+
+        # Embed the input tokens using the input embedding layer. Shape: [B, L, D]
+        x = self.input_embedding(x)
+        
+        # Add the positional embeddings to the tokens
+        # Hint: Make sure this works for sequences of different lengths
+        x = x + self.positional_embedding[0:L]
+
+        # Define the causal mask for the transformer trunk. 
+        # False = masked-out, True = not masked. Shape: [1, L, L]
+        # Hint: What shape should the mask have such that each token can attend to itself and
+        # all previous tokens, but not to any future tokens?
+        causal_mask = torch.tril(torch.ones(L, L))
+        causal_mask = causal_mask.unsqueeze(0).bool()
+            
+        # Forward pass through Transformer trunk
+        # Hint: Make sure to pass the causal mask to the transformer trunk too
+        x = self.trunk(x, causal_mask)
+        
+        # Pass to the output normalization and output projection layer to compute the logits
+        x = self.to_logits(self.out_norm(x))
+
+        # Return the logits
+        return x
+
+    def compute_ce_loss(self, logits: torch.Tensor, target_seq: torch.LongTensor, padding_idx: int = -100) -> torch.Tensor:
+        """
+        Compute the cross-entropy loss given logits and target labels, ignoring padding tokens.
+
+        Args:
+             logits: Tensor of shape (B, L, vocab_size)
+             target_seq: Tensor of shape (B, L) containing the target token indices.
+             padding_idx: The index of the [PAD] token that should be ignored in the loss computation.
+        Returns:
+             A scalar loss value.
+        """
+        # Compute the cross-entropy loss
+        # Hint: Remember to ignore the padding token index in the loss calculation
+        
+        logits = logits.reshape(-1, logits.size(-1)) # resizing
+        target_seq = target_seq.reshape(-1)
+        loss = F.cross_entropy(logits, target_seq, ignore_index = padding_idx)
+        return loss
+
+    def forward(self, data_dict: Dict[str, Any]) -> Tuple[torch.Tensor, Dict[str, Any]]:
+        """
+        Forward pass through the model.
+
+        Args:
+            data_dict: A dictionary containing the input sequence.
+        Returns:
+            The loss and a dictionary containing the perplexity metric.
+        """
+        seq = data_dict[self.seq_read_key] # Shape (B, L+1): e.g. [SOS], T_1, T_2, ..., T_L, [EOS], [PAD] ...
+        input_seq = seq[:, :-1] # Shape (B, L): e.g. [SOS], T_1, T_2, ..., T_L, [EOS], [PAD] ... (with the last token dropped)
+        target_seq = seq[:, 1:] # Shape (B, L): e.g. T_1, T_2, ..., T_L, [EOS], [PAD] ... (with the first token dropped)
+
+        # Forward pass through the model and compute loss
+        logits = self.forward_model(input_seq)
+        loss = self.compute_ce_loss(logits, target_seq, padding_idx=self.padding_idx)
+
+        metrics_dict = {'ppl': torch.exp(loss)} # Perplexity
+        return loss, metrics_dict
+
+    @torch.no_grad()
+    def generate(
+            self, 
+            context: List[int] = [0], 
+            eos_idx: Optional[int] = None, 
+            temp: float = 1.0, 
+            top_p: float = 0.0, 
+            top_k: float = 0.0,
+        ) -> torch.Tensor:
+        """
+        Generate a sequence autoregressively given an initial context.
+
+        Args:
+            context: List of token indices to start the sequence.
+            eos_idx: Optional [EOS] index of the end-of-sequence token. If None, the 
+                model will generate until the maximum sequence length.
+            temp: Temperature for sampling.
+            top_p: Nucleus sampling threshold.
+            top_k: Top-k sampling threshold.
+        Returns:
+            A tensor of shape (1, L) containing the generated sequence.
+        """
+        was_training = self.training
+        self.eval()
+
+        # Initialize the sequence with the start-of-sequence token
+        current_tokens = torch.tensor([context], dtype=torch.long, device=self.device)
+        for _ in range(self.max_seq_len - len(context)):
+
+            # Run a forward pass through the model to get the logits
+            logits = self.forward_model(current_tokens)
+
+            # Keep only the last token's logits and sample the next token
+            # Hint: Use the sample_tokens function from utils/sampling.py
+            # Make sure to pass the temperature, top_k and top_p arguments
+            next_token = sample_tokens(logits[:, -1, :], temp=temp, top_k=top_k, top_p=top_p)
+
+            # Concatenate the new token to the current_tokens sequence
+            current_tokens = torch.cat([current_tokens, next_token], dim=1)
+
+            # Break if the end-of-sequence token is generated
+            if eos_idx is not None and next_token.item() == eos_idx:
+                break
+
+        if was_training:
+            self.train()
+        
+        return current_tokens
